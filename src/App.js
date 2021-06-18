15f5a07b18122bde1a0fbdd2eb0c5be500fbec6e
Jenna Groth <jenna.groth18@alumni.colostate.edu>
2021-06-18 15:26:46 -0600

    "Initialized react app, rendering data from api"

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  const [display, setDisplay] = useState('loading');
+  const [allCommunities, setAllCommuniites] = useState('');
+
+  useEffect(() => {
+    axios.get('https://u50g7n0cbj.execute-api.us-east-1.amazonaws.com/v2/locations?limit=10&page=1&offset=0&sort=desc&radius=1000&order_by=lastUpdated&entity=community&dumpRaw=false')
+      .then((response) => {
+        setAllCommuniites(response.data.results);
+        console.log(response.data.results);
+        setDisplay('ready');
+      })
+      .catch((error) => {
+        throw error;
+      });
+  }, []);
+
+
+  if (display === 'loading') {
+    return (
+      <div className="App">
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
+
+  if (display === 'ready') {
+    return (
+      <div className="App">
+        {allCommunities.map((community) => {
+          return(
+            <ul>
+              <li>
+                <span>{community.name}</span>
+                <span>{community.parameters[0].average}</span>
+              </li>
+            </ul>
+          )
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
