7c64985d118f7332fecc07dbc38817bcd8c649b7
Jenna Groth <jenna.groth18@alumni.colostate.edu>
2021-06-20 11:10:32 -0600

    "Added high/low sorting functionality"

--- src/App.js
+++ src/App.js
@@ -5,13 +5,28 @@ import axios from 'axios';
 
 const App = () => {
   const [display, setDisplay] = useState('loading');
-  const [allCommunities, setAllCommuniites] = useState('');
+  const [allCommunities, setAllCommunities] = useState('');
+  const [highCommunities, setHighCommunities] = useState([]);
+  const [lowCommunities, setLowCommunities] = useState([]);
 
   useEffect(() => {
-    axios.get('https://u50g7n0cbj.execute-api.us-east-1.amazonaws.com/v2/locations?limit=10&page=1&offset=0&sort=desc&radius=1000&order_by=lastUpdated&entity=community&dumpRaw=false')
+    axios.get('https://u50g7n0cbj.execute-api.us-east-1.amazonaws.com/v2/locations?limit=16000&page=1&offset=0&sort=desc&radius=1000&order_by=lastUpdated&entity=community&dumpRaw=false')
       .then((response) => {
-        setAllCommuniites(response.data.results);
-        console.log(response.data.results);
+        setAllCommunities(response.data.results);
+        // console.log(response.data.results);
+        response.data.results.forEach((community) => {
+          var category = 'low';
+          community.parameters.forEach((parameter) => {
+            if (parameter.unit === 'µg/m³' && parameter.average >= 5) {
+              category = 'high';
+            }
+          })
+          if (category === 'high') {
+            setHighCommunities(highCommunities => [...highCommunities, community]);
+          } else {
+            setLowCommunities(lowCommunities => [...lowCommunities, community]);
+          }
+        })
         setDisplay('ready');
       })
       .catch((error) => {
@@ -31,7 +46,19 @@ const App = () => {
   if (display === 'ready') {
     return (
       <div className="App">
-        {allCommunities.map((community) => {
+        <h3>High Communities: {highCommunities.length}</h3>
+        {highCommunities.map((community) => {
+          return(
+            <ul>
+              <li>
+                <span>{community.name}</span>
+                <span>{community.parameters[0].average}</span>
+              </li>
+            </ul>
+          )
+        })}
+        <h3>Low Communities: {lowCommunities.length}</h3>
+        {lowCommunities.map((community) => {
           return(
             <ul>
               <li>
